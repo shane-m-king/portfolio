01e44684f5f3479724c5c70a3dded5bdda37ce27
Shane King <shane.m.king94@gmail.com>
2025-12-15 15:37:31 -0800

    Mobile Spacing Adjustment

--- components/ProjectImageCarousel.tsx
+++ components/ProjectImageCarousel.tsx
@@ -124,6 +124,7 @@ export default function ProjectImageCarousel({ images, alt }: Props) {
       {/* LIGHTBOX MODAL */}
       {isZoomed && (
         <div
+        {...handlers}
           className="fixed inset-0 z-40 flex items-center justify-center bg-black/70 p-[calc(var(--tetris-unit)*0.3)]"
           onClick={() => setIsZoomed(false)}
           aria-modal="true"
